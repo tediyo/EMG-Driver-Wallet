dfc770bf93d69aaf1f4222e8d28d2d9716245abd
Tewodros <thedronberhanu05@gmail.com>
2025-02-27 00:58:50 +0300

    api and service provider are updated

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WalletController;
+use App\Http\Controllers\SecondLargestController;
+
+Route::post('/second-largest', [SecondLargestController::class, 'getSecondLargest']);// For algorithm challenge
+
 
 Route::post('/wallet/topup', [WalletController::class, 'topUp']);
 Route::get('/wallet/balance/{driver_id}', [WalletController::class, 'checkBalance']);
